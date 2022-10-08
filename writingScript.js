e8b3c24d92edd13aaccf917414f481e1fc95256d
Bekkiconfetti <80005582+Bekkiconfetti@users.noreply.github.com>
2022-10-08 11:03:44 +0200

    legibitlity add content writing

---
+++ writingScript.js
@@ -0,0 +1,13 @@
+
+let otherImages=document.getElementsByClassName('otherImage');
+
+//SCRAMBLING PROJECTS BEGINNING
+//let startButton=document.getElementById('aboutImage');
+const width  = window.innerWidth || document.documentElement.clientWidth ||
+document.body.clientWidth;
+
+
+if(width<800){
+	//document.body.style.backgroundColor = 'aqua';
+	document.getElementById('spacing').style.display ='block';
+					}
