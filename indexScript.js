8338697bf69dcc66b6fcbed2ee8e78533064f60f
Bekkiconfetti <80005582+Bekkiconfetti@users.noreply.github.com>
2024-08-02 00:41:27 +0200

    optImages
    touch-ups index, better image management

--- indexScript.js
+++ indexScript.js
@@ -4,6 +4,7 @@
 
 let coverImages=document.getElementsByClassName('coverImage');
 let startButton=document.getElementById('start');
+let shuffleButton=document.getElementById('shuffle');
 
 //SCRAMBLING PROJECTS BEGINNING
 //let startButton=document.getElementById('aboutImage');
@@ -15,7 +16,7 @@ let scramble = function(event){
 					for(let i=0;i<coverImages.length;i++){
 					coverImages[i].style.display= 'block';
 					coverImages[i].style.position='absolute';
-					coverImages[i].style.top= Math.floor(Math.random()* (50 - 8 + 1) + 8)+'%';
+					coverImages[i].style.top= Math.floor(Math.random()* (50 - 8 + 1) + 2)+'%';
 					coverImages[i].style.left=Math.floor(Math.random()* (70 - + 1) )+'%';
 					coverImages[i].style.zIndex=Math.floor(Math.random()*(4-1) + 1);
 /*
@@ -38,6 +39,7 @@ if(width<800){
 else{
 
 			startButton.addEventListener('click',scramble);
+			shuffleButton.addEventListener('click',scramble);
 				window.addEventListener('load',scramble);
 				window.addEventListener('pageshow',scramble);
 
